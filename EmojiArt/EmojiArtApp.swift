d330c9693b5cd95a656592cccc9a2b52fe0ea808
NHUJI <cnzhouchang@gmail.com>
2023-04-01 13:00:56 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by huhu on 2023/4/1.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
